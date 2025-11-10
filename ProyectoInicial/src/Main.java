f228bfba8a9c0d972da6642c8e4a21d5a8b37a34
raimonortirovira <raimonortirovira@gmail.com>
2025-11-10 12:01:17 +0100

    update

--- ProyectoInicial/src/Main.java
+++ ProyectoInicial/src/Main.java
@@ -1,3 +1,16 @@
 public class Main {
-    
+
+    // esto es un comentario
+    /*
+    este comentario
+    se puede dividir en
+    varias linea de código
+     */
+    //TODO me falta esto
+
+    public static void main(String[] args){
+        System.out.print("Este es mi primer programa en Java\n");
+        System.out.print("Esta es mi segunda linea del programa");
+    }
+    //TODO esto tambien esta pendiente
 }
