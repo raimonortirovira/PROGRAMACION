9fe53c805b5a62f1ca28ee9d558dd41fceb38eaf
raimonortirovira <raimonortirovira@gmail.com>
2025-11-13 18:20:57 +0100

    update

---
+++ TEMA 3/Ejerciciosclase/src/Main.java
@@ -0,0 +1,20 @@
+package ejercicio1
+
+public class Main {
+    /*
+    1. Crea un programa de adivinación de números El programa genera de manera
+aleatoria un número entre 1 y 50. El usuario debe adivinar el número en un
+máximo de 7 intentos. Después de cada intento, indica si el número es mayor
+o menor que el elegido. Si el usuario acierta, mostrar un aviso con el número
+de intentos utilizado; si no, mostrar un aviso de que debe introducir
+nuevamente un número.
+    1 - Generar un aleatorio entre 1-50
+    2 -  7 intentos / repeticiones
+    3 - indicar si el número es mayor o menor del elegido
+    4 - Mostrar un aviso con el número de intentos utilizado
+
+     */
+    public static void main (String[] args) {
+        
+    }
+}
