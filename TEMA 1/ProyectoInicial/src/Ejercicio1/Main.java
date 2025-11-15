be8cdb1577b9ef3a8eaca12e52c56f0d92df01a0
raimonortirovira <raimonortirovira@gmail.com>
2025-11-15 11:51:30 +0100

    update

--- TEMA 1/ProyectoInicial/src/Ejercicio1/Main.java
+++ TEMA 1/ProyectoInicial/src/Ejercicio1/Main.java
@@ -1,3 +1,5 @@
+package Ejercicio1;
+
 public class Main {
 
     // esto es un comentario
@@ -12,26 +14,51 @@ public class Main {
         System.out.print("Primer programa en Java\n");
         String miNombre = "Borja"; // null
 
-
         final String DNI = "23423434";
         Double alturaCompleja = 21.76;
-        double altura 1.71;
+
+        // **ERROR CORREGIDO 1:** La línea 'double altura 1.71;' fue eliminada.
+        // La eliminé para evitar la duplicación de la variable 'altura' más abajo
+
         char letraDNI = 'L';
         int edad = 41;
+
+        // ÚNICA DECLARACIÓN DE ALTURA: Eliminada la duplicación de la línea 22 original
         double altura = 1.71234234234234;
+
         float peso = 70.76f;
+        Boolean experienciaCompleja = false;
         boolean experiencia = true;
+
+        // **ERROR CORREGIDO 2:** ¡Faltaban puntos y coma (;) al final de todas estas líneas!
+        edad= 23;
         edad++;
-        edad++;
-        letraDNI = 'B';
-        letraDNI = 'I';
+        letraDNI = 109;
         miNombre = "Borja Martin Herrera";
         altura = 1.75;
         peso = 67.5f;
         experiencia = false;
+
+        // **********************
+        // IMPRESIONES
+        // **********************
+
         System.out.println("Mi nombre es "+miNombre);
         System.out.println("La letra de mi dni es =" + letraDNI);
         System.out.println("Mi edad es " + edad);
         System.out.println("Mi altura es de "+altura + " cm");
         System.out.println("Mi peso es de "+peso + " kg");
         System.out.println("Mi experiencia es: "+experiencia);
+        System.out.println("Mi DNI es:" + DNI);
+        System.out.println("El valor del numero Pi es " +Math.PI );
+        System.out.println("El valor del numero E " +Math.E);
+        System.out.println("El valor maximo de un int es " +Integer.MAX_VALUE);
+        System.out.println("El valor minimo de un int es " +Integer.MIN_VALUE);
+    }
+
+
+    public void nombreMetodo() {
+        int edadMetodo = 12;
+        System.out.println(edadMetodo);
+    }
+}
